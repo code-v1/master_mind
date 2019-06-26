8d7ce4e2aa910ccf9a8670e2dae1222451a2d416
Eric Tovar <tovareric@gmail.com>
2019-06-26 16:58:41 -0500

    rendering components and updating state

--- src/App.js
+++ src/App.js
@@ -5,16 +5,59 @@ import ColorPicker from './components/ColorPicker/ColorPicker'
 import GameTimer from './components/GameTimer/GameTimer'
 import NewGameButton from './components/NewGameButton/NewGameButton'
 
+const colors = ['#7CCCE5', '#FDE47F', '#E04644', '#B576AD'];
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      selColorIdx: 0,
+      guesses: [this.getNewGuess(), this.getNewGuess()],
+      code:this.genCode()
+    };
+  }
+
+  getNewGuess() {
+    return{
+      // code: [null, null, null, null],
+      code: [3, 2, 1, 0],
+      score: {
+        perfect: 0,
+        almost: 0
+      }
+    };
+  }
+
+  genCode() {
+    return new Array(4).fill().map(() => Math.floor(Math.random() * colors.length));
+  }
+
   render() {
     return (
       <div className="App">
+      <button onClick={() =>
+        this.setState((state) => {
+          return {
+          
+          selColorIdx: ++state.selColorIdx % 4
+          }
+        })
+      }>
+        Next Color
+      </button>
+      Selected color: {colors[this.state.selColorIdx]}
         <header className="App-header">React Mastermind</header>
       <div className='flex-g'>
-        <GameBoard  />
+        <GameBoard 
+          colors={colors}
+          guesses={this.state.guesses}
+        />
         <div>
-        <ColorPicker />
+        <ColorPicker 
+        colors={colors}
+        selColorIdx={this.state.selColorIdx} 
+        />
         <GameTimer />
         <NewGameButton />
         </div>
