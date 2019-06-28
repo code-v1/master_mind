ba6fa9ef5e7072a4a6056ca7f9812b059ab26969
Eric Tovar <tovareric@gmail.com>
2019-06-28 10:34:34 -0500

    adjusting row to board

--- src/components/GuessRow/GuessRow.jsx
+++ src/components/GuessRow/GuessRow.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import GuessPegs from '../GuessPegs/GuessPegs'
 import GuessScore from '../GuessScore/GuessScore'
 import ScoreButton from '../ScoreButton/ScoreButton'
+import styles from './GuessRow.module.css'
 
 const GuessRow = (props) => (
-    <div className='flex-g'>
-    <div style={{color: props.currentGuess ? 'black' : 'lightgrey'}}>{props.rowIdx + 1}</div>
+    <div className={styles.GuessRow}>
+    <div 
+    className={styles.rowNum}
+    style={{color: props.currentGuess ? 'black' : 'lightgrey'}}>{props.rowIdx + 1}
+    >
+    {props.rowIdx + 1}
+    </div>
     <GuessPegs 
         colors={props.colors}
         code={props.guess.code}
