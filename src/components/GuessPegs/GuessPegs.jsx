04e926fcda8e2ba663341893f8da9b209bbed48f
Eric Tovar <tovareric@gmail.com>
2019-06-28 16:01:43 -0500

    get color select for peg

--- src/components/GuessPegs/GuessPegs.jsx
+++ src/components/GuessPegs/GuessPegs.jsx
@@ -9,11 +9,27 @@ import './GuessPegs.css';
 
 const GuessPegs = (props) => (
     <div className='GuessPegs'>
-        <GuessPeg color={props.colors[props.code[0]]} />
-        <GuessPeg color={props.colors[props.code[1]]} />
-        <GuessPeg color={props.colors[props.code[2]]} />
-        <GuessPeg color={props.colors[props.code[3]]} />
+        <GuessPeg 
+        color={props.colors[props.code[0]]} 
+        currentGuess={props.currentGuess}
+        />
+        <GuessPeg 
+        color={props.colors[props.code[1]]} 
+        currentGuess={props.currentGuess}
+        />
+        <GuessPeg 
+        color={props.colors[props.code[2]]}
+        currentGuess={props.currentGuess}
+        />
+        <GuessPeg 
+        color={props.colors[props.code[3]]}
+        currentGuess={props.currentGuess}
+        />
     </div>
 );
+
+
+
+
 
 export default GuessPegs;
