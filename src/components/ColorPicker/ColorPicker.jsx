795793d4d800160f10562d40bc6af52ed824efd3
Eric Tovar <tovareric@gmail.com>
2019-06-28 15:59:23 -0500

    handleColorSelector

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -13,6 +13,7 @@ const ColorPicker = (props) => (
           backgroundColor: props.selColorIdx === idx ? 'white' : color,
           borderColor: color
         }}
+        onClick={() => props.handleColorSelection(idx)}
       />
     )}
   </div>
