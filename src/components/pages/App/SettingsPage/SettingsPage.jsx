e54a64582115bf2c1ce97b676ab9cf8993b035ed
Eric Tovar <tovareric@gmail.com>
2019-07-01 16:58:14 -0500

    making settings page

---
+++ src/components/pages/App/SettingsPage/SettingsPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+const SettingsPage = (props) => {
+    return (
+    <>
+    <Link to ='/'>HOME</Link>
+    <h1>Settings Page</h1>
+
+    </>
+    );
+
+}
+
+
+
+
+
+export default SettingsPage;
