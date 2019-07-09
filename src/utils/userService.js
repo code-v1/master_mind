74cc0123a3077fdc963807af50d6ca879714f162
Eric Tovar <tovareric@gmail.com>
2019-07-09 12:41:47 -0500

    setting up auth

---
+++ src/utils/userService.js
@@ -0,0 +1,22 @@
+
+import tokenService from './tokenService';
+
+const BASE_URL = '/api/users/';
+
+function signup(user) {
+  return fetch(BASE_URL + 'signup', {
+    method: 'POST',
+    headers: new Headers({'Content-Type': 'application/json'}),
+    body: JSON.stringify(user)
+  })
+  .then(res => {
+    if (res.ok) return res.json();
+    // Probably a duplicate email
+    throw new Error('Email already taken!');
+  })
+  .then(({token}) => tokenService.setToken(token));
+}
+
+export default {
+  signup
+};
