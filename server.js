69f3ec2351796fd53926d69e04924df175ef74cd
Eric Tovar <tovareric@gmail.com>
2019-07-09 12:45:56 -0500

    server set up

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
+
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon')
-const logger = require('morgan')
-
+const favicon = require('serve-favicon');
+const logger = require('morgan');
 
 const app = express();
 
+require('dotenv').config();
+require('./config/database');
+
 app.use(logger('dev'));
 app.use(express.json());
+
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Put your API routes here before the 'catch all' route!
+// Put API routes here, before the "catch all" route
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/scores', require('./routes/api/scores'));
 
-app.get('/*', function (req, res){
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
+// The following "catch all" route (note the *)is necessary
+// for a SPA's client-side routing to properly work 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
+// Configure to use port 3001 instead of 3000 during
+// development to avoid collision with React's dev server
+const port = process.env.PORT || 3001;
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, function(){
-    console.log(`Express ap running on port ${port}`)
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
 });
